54178fef7c10b08eb2d74bbdac8f24a413d82455
RDP77 <ravidwiputra@gmail.com>
2020-04-28 23:29:28 +0700

    Is Feeling Good

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,58 @@
-@extends('layouts.app')
+@extends('layouts_frontend._main')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<section class="contact-section">
+    <div class="container-fluid">
+        <div class="contact-warp">
+            <div class="section-title mb-5">
+                <h2>{{ __('Register') }}</h2>
             </div>
+            <form method="POST" action="{{ route('register') }}" class="contact-from">
+                @csrf
+                <div class="row">
+                    <div class="col-md-12">
+                        {{-- Name --}}
+                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
+                            name="name" placeholder="Your name" value="{{ old('name') }}" required autocomplete="name"
+                            autofocus>
+
+                        @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+
+                        {{-- Email --}}
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                            name="email" placeholder="Your e-mail" value="{{ old('email') }}" required
+                            autocomplete="email">
+
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+
+                        {{-- Password --}}
+                        <input id="password" type="password"
+                            class="form-control @error('password') is-invalid @enderror" name="password" required
+                            autocomplete="new-password" placeholder="Your Password">
+
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+
+                        {{-- Confirm Password --}}
+                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation"
+                            required autocomplete="new-password" placeholder="Confirm Password">
+
+                        <button type="submit" class="site-btn mt-3">{{ __('Register') }}</button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
-</div>
+</section>
 @endsection
--- resources/views/layouts_frontend/_main.blade.php
+++ resources/views/layouts_frontend/_main.blade.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
-<html lang="zxx">
+<html lang="id">
+
 <head>
-    <title>SolMusic | HTML Template</title>
+    <title>Perpustakaan Dryas</title>
     <meta charset="UTF-8">
     <meta name="description" content="SolMusic HTML Template">
     <meta name="keywords" content="music, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('assets/img/favicon.ico') }}" rel="shortcut icon"/>
-    @include('layouts_frontend._css') 
+    <link href="{{ asset('assets/img/favicon.ico') }}" rel="shortcut icon" />
+    @include('layouts_frontend._css')
 </head>
+
 <body>
     <div id="preloder">
         <div class="loader"></div>
@@ -19,89 +21,43 @@
             <img src="{{ asset('assets/img/logo.png') }}" alt="">
         </a>
         <div class="header-right">
-            <a href="#" class="hr-btn">Help</a>
-            <span>|</span>
             <div class="user-panel">
-            @if (Auth::user() != null)
+                @if (Auth::user() != null)
                 <a href="{{ route('register') }}" class="register">Hy, {{ Auth::user()->name }}</a>
-            @else
+                @else
                 <a href="{{ route('login') }}" class="login">Login</a>
+                <span>|</span>
                 <a href="{{ route('register') }}" class="register">Create an account</a>
-            @endif
-            </div> 
+                @endif
+            </div>
         </div>
         <ul class="main-menu">
             <li><a href="{{ route('welcome') }}">Home</a></li>
-            <li><a href="{{ route('buku_katalog') }}">Katalog</a></li>
-            <li><a href="#">Pages</a>
-                <ul class="sub-menu">
-                    <li><a href="category.html">Category</a></li>
-                    <li><a href="playlist.html">Playlist</a></li>
-                    <li><a href="artist.html">Artist</a></li>
-                    <li><a href="blog.html">Blog</a></li>
-                    <li><a href="contact.html">Contact</a></li>
-                </ul>
-            </li>
-            <li><a href="blog.html">News</a></li>
-            <li><a href="contact.html">Contact</a></li>
+            <li><a href="{{ route('buku_katalog') }}">Catalog</a></li>
+            <li><a href="javascript:void(0)">About</a></li>
+            <li><a href="javascript:void(0)">Our Team</a></li>
         </ul>
     </header>
     @yield('content')
-  <footer class="footer-section">
+    <footer class="footer-section">
         <div class="container">
             <div class="row">
-                <div class="col-xl-6 col-lg-7 order-lg-2">
-                    <div class="row">
-                        <div class="col-sm-4">
-                            <div class="footer-widget">
-                                <h2>About us</h2>
-                                <ul>
-                                    <li><a href="">Our Story</a></li>
-                                    <li><a href="">Sol Music Blog</a></li>
-                                    <li><a href="">History</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-sm-4">
-                            <div class="footer-widget">
-                                <h2>Products</h2>
-                                <ul>
-                                    <li><a href="">Music</a></li>
-                                    <li><a href="">Subscription</a></li>
-                                    <li><a href="">Custom Music</a></li>
-                                    <li><a href="">Footage</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-sm-4">
-                            <div class="footer-widget">
-                                <h2>Playlists</h2>
-                                <ul>
-                                    <li><a href="">Newsletter</a></li>
-                                    <li><a href="">Careers</a></li>
-                                    <li><a href="">Press</a></li>
-                                    <li><a href="">Contact</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-6 col-lg-5 order-lg-1">
+
+                <div class="col-xl-6">
                     <img src="{{ asset('assets/img/logo.png') }}" alt="">
-                    <div class="copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></div>
-                    <div class="social-links">
-                        <a href=""><i class="fa fa-instagram"></i></a>
-                        <a href=""><i class="fa fa-pinterest"></i></a>
-                        <a href=""><i class="fa fa-facebook"></i></a>
-                        <a href=""><i class="fa fa-twitter"></i></a>
-                        <a href=""><i class="fa fa-youtube"></i></a>
+                    <div class="copyright">
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        Copyright &copy;<script>
+                            document.write(new Date().getFullYear());
+                        </script> All rights reserved | This template is made with <i class="fa fa-heart-o"
+                            aria-hidden="true"></i> by Five Team
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                     </div>
                 </div>
             </div>
         </div>
     </footer>
-    @include('layouts_frontend._script') 
+    @include('layouts_frontend._script')
 </body>
+
 </html>
