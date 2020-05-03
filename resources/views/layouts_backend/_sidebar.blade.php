3933f3b58dbe8485092163d2ef4a3b91a19e0195
RDP77 <ravidwiputra@gmail.com>
2020-05-04 00:39:01 +0700

    Tambahan

--- resources/views/layouts_backend/_sidebar.blade.php
+++ resources/views/layouts_backend/_sidebar.blade.php
@@ -1,7 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
-  <a href="index3.html" class="brand-link">
-    <span class="brand-text font-weight-bold text-center">Dryas Library</span>
+  <a href="javascript:void(0)" class="brand-link">
+    <i class="fa fa-book ml-4 mr-2"></i>
+    <span class="brand-text font-weight-light text-monospace">Dryas Library</span>
   </a>
 
   <!-- Sidebar -->
