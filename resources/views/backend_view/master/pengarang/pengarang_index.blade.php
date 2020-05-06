c48090f74d90aeba54815f5d1e9baa959373a537
AzrieL/Muttq <denyprasetyo41@gmail.com>
2020-05-06 17:52:30 +0700

    123

--- resources/views/backend_view/master/pengarang/pengarang_index.blade.php
+++ resources/views/backend_view/master/pengarang/pengarang_index.blade.php
@@ -29,7 +29,7 @@
               <!-- form start -->
 
                 <div class="card-body">
-                    <table id="example1" class="table table-bordered table-striped">
+                    <table id="example1 " class="table table-bordered table-striped">
                         <thead>
                             <tr>
                                 <td>no</td>
