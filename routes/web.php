8e29b88bf117e35f1448aba6d5657ef471b2f846
RDP77 <ravidwiputra@gmail.com>
2020-06-27 00:28:17 +0700

    Steins Gate

--- routes/web.php
+++ routes/web.php
@@ -28,20 +28,12 @@ Route::get('/catalog', 'buku_katalogController@buku_katalog')->name('buku_katalo
 Auth::routes();
 
 Route::get('/dashboard', 'HomeController@index')->name('home');
-
-Route::get('/user', 'userController@index')->name('user_index');
-Route::get('/user_create', 'userController@create')->name('user_create');
-Route::get('/user_save', 'userController@save')->name('user_save');
-Route::get('/user_edit', 'userController@edit')->name('user_edit');
-Route::get('/user_update', 'userController@update')->name('user_update');
-Route::get('/user_hapus', 'userController@hapus')->name('user_hapus');
-Route::get('/user_perpanjang', 'userController@perpanjang')->name('user_perpanjang');
-
+// Profile
 Route::get('/profile', 'userController@profile')->name('profile_index');
 Route::get('/profile_edit', 'userController@profileedit')->name('profile_edit');
 Route::post('/profile_update', 'userController@profileupdate')->name('profile_update');
 Route::get('/idcard_print', 'userController@profileprint')->name('profile_print');
-
+// Profile
 Route::get('/forgot_password', 'Auth\ForgotPasswordController@index')->name('forgot_password_index');
 Route::get('/change_password', 'Auth\ForgotPasswordController@changepassword')->name('forgot_password');
 Route::get('/password_reset', 'Auth\ForgotPasswordController@logout')->name('password_reset');
@@ -93,39 +85,52 @@ Route::group(['middleware' => 'roles'], function () {
     Route::get('/rak_buku_get_kode', 'rak_bukuController@get_kode')->name('rak_buku_get_kode');
     Route::get('/rak_buku_dt_save', 'rak_bukuController@save_dt')->name('rak_buku_dt_save');
     Route::get('/rak_buku_dt_delete', 'rak_bukuController@deletes_dt')->name('rak_buku_dt_delete');
+    // Fakultas
+    Route::get('/fakultas', 'fakultasController@index')->name('fakultas_index');
+    Route::get('/fakultas_create', 'fakultasController@create')->name('fakultas_create');
+    Route::get('/fakultas_save', 'fakultasController@save')->name('fakultas_save');
+    Route::get('/fakultas_edit', 'fakultasController@edit')->name('fakultas_edit');
+    Route::get('/fakultas_update', 'fakultasController@update')->name('fakultas_update');
+    Route::get('/fakultas_hapus', 'fakultasController@hapus')->name('fakultas_hapus');
+    // Buku
+    Route::get('/buku', 'bukuController@index')->name('buku_index');
+    Route::get('/buku_create', 'bukuController@create')->name('buku_create');
+    Route::get('/buku_save', 'bukuController@save')->name('buku_save');
+    Route::get('/buku_edit', 'bukuController@edit')->name('buku_edit');
+    Route::get('/buku_update', 'bukuController@update')->name('buku_update');
+    Route::get('/buku_hapus', 'bukuController@hapus')->name('buku_hapus');
+    // Peminjaman
+    Route::get('/transaksi_peminjaman', 'transaksi_peminjamanController@index')
+        ->name('transaksi_peminjaman_index');
+    Route::get('/transaksi_peminjaman_create', 'transaksi_peminjamanController@create')
+        ->name('transaksi_peminjaman_create');
+    Route::get('/transaksi_peminjaman_save', 'transaksi_peminjamanController@save')
+        ->name('transaksi_peminjaman_save');
+    Route::get('/transaksi_peminjaman_edit', 'transaksi_peminjamanController@edit')
+        ->name('transaksi_peminjaman_edit');
+    Route::get('/transaksi_peminjaman_update', 'transaksi_peminjamanController@update')
+        ->name('transaksi_peminjaman_update');
+    Route::get('/transaksi_peminjaman_hapus', 'transaksi_peminjamanController@hapus')
+        ->name('transaksi_peminjaman_hapus');
+    Route::get('/transaksi_peminjaman_get_data_buku', 'transaksi_peminjamanController@get_data_buku')
+        ->name('transaksi_peminjaman_get_data_buku');
+    Route::get('/transaksi_peminjaman_get_data_buku_remove', 'transaksi_peminjamanController@get_data_buku_remove')
+        ->name('transaksi_peminjaman_get_data_buku_remove');
+    // Pengembalian
+    Route::get('/transaksi_pengembalian', 'transaksi_pengembalianController@index')
+        ->name('transaksi_pengembalian_index');
+    Route::get('/transaksi_pengembalian_create', 'transaksi_pengembalianController@create')
+        ->name('transaksi_pengembalian_create');
+    Route::get('/transaksi_pengembalian_save', 'transaksi_pengembalianController@save')
+        ->name('transaksi_pengembalian_save');
+    Route::get('/transaksi_pengembalian_edit', 'transaksi_pengembalianController@edit')
+        ->name('transaksi_pengembalian_edit');
+    Route::get('/transaksi_pengembalian_update', 'transaksi_pengembalianController@update')
+        ->name('transaksi_pengembalian_update');
+    Route::get('/transaksi_pengembalian_hapus', 'transaksi_pengembalianController@hapus')
+        ->name('transaksi_pengembalian_hapus');
+    Route::get('/transaksi_pengembalian_get_data_peminjaman', 'transaksi_pengembalianController@get_data_peminjaman')
+        ->name('transaksi_pengembalian_get_data_peminjaman');
+    Route::get('/transaksi_pengembalian_get_data_pengembalian', 'transaksi_pengembalianController@get_data_pengembalian')
+        ->name('transaksi_pengembalian_get_data_pengembalian');
 });
-
-Route::get('/buku', 'bukuController@index')->name('buku_index');
-Route::get('/buku_create', 'bukuController@create')->name('buku_create');
-Route::get('/buku_save', 'bukuController@save')->name('buku_save');
-Route::get('/buku_edit', 'bukuController@edit')->name('buku_edit');
-Route::get('/buku_update', 'bukuController@update')->name('buku_update');
-Route::get('/buku_hapus', 'bukuController@hapus')->name('buku_hapus');
-
-Route::get('/fakultas', 'fakultasController@index')->name('fakultas_index');
-Route::get('/fakultas_create', 'fakultasController@create')->name('fakultas_create');
-Route::get('/fakultas_save', 'fakultasController@save')->name('fakultas_save');
-Route::get('/fakultas_edit', 'fakultasController@edit')->name('fakultas_edit');
-Route::get('/fakultas_update', 'fakultasController@update')->name('fakultas_update');
-Route::get('/fakultas_hapus', 'fakultasController@hapus')->name('fakultas_hapus');
-
-
-// peminjaman
-Route::get('/transaksi_peminjaman', 'transaksi_peminjamanController@index')->name('transaksi_peminjaman_index');
-Route::get('/transaksi_peminjaman_create', 'transaksi_peminjamanController@create')->name('transaksi_peminjaman_create');
-Route::get('/transaksi_peminjaman_save', 'transaksi_peminjamanController@save')->name('transaksi_peminjaman_save');
-Route::get('/transaksi_peminjaman_edit', 'transaksi_peminjamanController@edit')->name('transaksi_peminjaman_edit');
-Route::get('/transaksi_peminjaman_update', 'transaksi_peminjamanController@update')->name('transaksi_peminjaman_update');
-Route::get('/transaksi_peminjaman_hapus', 'transaksi_peminjamanController@hapus')->name('transaksi_peminjaman_hapus');
-Route::get('/transaksi_peminjaman_get_data_buku', 'transaksi_peminjamanController@get_data_buku')->name('transaksi_peminjaman_get_data_buku');
-Route::get('/transaksi_peminjaman_get_data_buku_remove', 'transaksi_peminjamanController@get_data_buku_remove')->name('transaksi_peminjaman_get_data_buku_remove');
-
-
-Route::get('/transaksi_pengembalian', 'transaksi_pengembalianController@index')->name('transaksi_pengembalian_index');
-Route::get('/transaksi_pengembalian_create', 'transaksi_pengembalianController@create')->name('transaksi_pengembalian_create');
-Route::get('/transaksi_pengembalian_save', 'transaksi_pengembalianController@save')->name('transaksi_pengembalian_save');
-Route::get('/transaksi_pengembalian_edit', 'transaksi_pengembalianController@edit')->name('transaksi_pengembalian_edit');
-Route::get('/transaksi_pengembalian_update', 'transaksi_pengembalianController@update')->name('transaksi_pengembalian_update');
-Route::get('/transaksi_pengembalian_hapus', 'transaksi_pengembalianController@hapus')->name('transaksi_pengembalian_hapus');
-Route::get('/transaksi_pengembalian_get_data_peminjaman', 'transaksi_pengembalianController@get_data_peminjaman')->name('transaksi_pengembalian_get_data_peminjaman');
-Route::get('/transaksi_pengembalian_get_data_pengembalian', 'transaksi_pengembalianController@get_data_pengembalian')->name('transaksi_pengembalian_get_data_pengembalian');
